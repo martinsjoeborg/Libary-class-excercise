6177d9bc08a9cfac511d69f4d569bdfcb6ca4990
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2022-12-06 14:20:34 +0100

    upp

---
+++ main/main.js
@@ -0,0 +1,27 @@
+class Books {
+    constructor(title, pages, writer, available) {
+        this.title = title;
+        this.pages = pages;
+        this.writer = writer;
+        this.available = available;
+
+    }
+
+    showInfo() {
+        let bookInfo = this.title + " is a book writen by " + this.writer + " and is " + this.pages + " pages long. " + this.available + ".";
+        return bookInfo;
+    }
+
+    borrowBook() {
+        return "You have borrowed " + this.title + ".";
+    }
+    returnBook() {
+        return "You have returned " + this.title + ".";
+    }
+}
+
+class Library {
+    constructor()
+}
+
+let potter = new Books("Harry Potter", )
